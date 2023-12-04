3556835cd21dc80186bd86f95beed602065f1618
AliyyahS <50198558+AliyyahS@users.noreply.github.com>
2023-12-04 18:14:32 +0000

    Clean up comments

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-// Function to get computer to randomly select rock, paper or scissors
+// Get computer to randomly select rock, paper or scissors
 
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 100);
@@ -12,7 +12,7 @@ function getComputerChoice() {
     }
 }
 
-// Function to get player to input rock, paper or scissors and make sure input is case insensitive
+// Get player to input rock, paper or scissors
 
 function getPlayerChoice () {
     let input;
@@ -24,24 +24,15 @@ function getPlayerChoice () {
     return input;
 }
 
-// Function to play a single round of rock, paper, scissors
+// Play a single round of rock, paper, scissors
 
 function playRound() {
-    // Calls function to get player's selection
-
     let playerSelection = getPlayerChoice();
-
-    // Calls function to get computer's selection
-
     let computerSelection = getComputerChoice();
-
-    // Outcome message after a single round of rock, paper, scissors
 
     const drawMessage = "It's a draw! You both picked " + playerSelection;
     const winMessage = "You win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection;
     const loseMessage = "Oh no, you lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection;
-
-    // Compares player's selection to computer's selection, prints the outcome message and returns the winner of the round
 
     if (playerSelection == computerSelection) {
         console.log(drawMessage);
@@ -55,17 +46,12 @@ function playRound() {
     }
 }
 
-// Function to play 5 rounds of rock, paper, scissors and tracks the score
+// Play 5 rounds of rock, paper, scissors and tracks the score
 
 function game() {
-
-    // Variables to track score
-
     let draw = 0;
     let playerWin = 0;
     let computerWin = 0;
-
-    // Calls function to play five rounds of game
     
     for (let i = 0; i < 5; i++) {
 
@@ -79,8 +65,6 @@ function game() {
             computerWin++;
         }
     }
-
-    // Compares the final results of 5 games
 
     if (playerWin == computerWin){
         return "The final result: It's a draw! You both scored " + playerWin;
