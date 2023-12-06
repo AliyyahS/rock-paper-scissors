8cd87d38ff9a396f14bd757b0c1f3469d98706f2
AliyyahS <50198558+AliyyahS@users.noreply.github.com>
2023-12-06 16:18:54 +0000

    Add UI elements

--- javascript.js
+++ javascript.js
@@ -1,78 +1,115 @@
-// Get computer to randomly select rock, paper or scissors
+let draw = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-function getComputerChoice() {
-    let randomChoice = Math.floor(Math.random() * 100);
+// Get computer to select rock, paper or scissors
 
-    if (randomChoice <= 33) {
+function getComputerSelection() {
+    let randomSelection = Math.floor(Math.random() * 100);
+
+    if (randomSelection <= 33) {
         return "rock";
-    } else if (randomChoice > 33 && randomChoice < 68) {
+    } else if (randomSelection > 33 && randomSelection < 68) {
         return "paper";
     } else {
         return "scissors";
     }
 }
 
-// Get player to input rock, paper or scissors
+// Play a round of rock, paper, scissors
 
-function getPlayerChoice () {
-    let input;
-
-    do {
-        input = prompt("Let's play! Choose rock, paper or scissors.").toLowerCase();
-    } while (input != "rock" && input != "paper" && input != "scissors");
-
-    return input;
-}
-
-// Play a single round of rock, paper, scissors
-
-function playRound() {
-    let playerSelection = getPlayerChoice();
-    let computerSelection = getComputerChoice();
+function playRound(playerSelection) {
+    let computerSelection = getComputerSelection();
+    let roundWinner = "";
 
     const drawMessage = "It's a draw! You both picked " + playerSelection;
     const winMessage = "You win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection;
     const loseMessage = "Oh no, you lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection;
 
     if (playerSelection == computerSelection) {
-        console.log(drawMessage);
-        return "draw";
+        message.textContent = drawMessage;
+        roundWinner = "draw";
     } else if ((playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "rock" && computerSelection == "scissors")) {
-        console.log(winMessage);
-        return "player";
+        message.textContent = winMessage;
+        roundWinner = "player";
     } else {
-        console.log(loseMessage);
-        return "computer";
+        message.textContent = loseMessage;
+        roundWinner = "computer";
+    }
+
+    score(roundWinner);
+
+    if (playerScore === 5) {
+        gameOver("player");
+    } else if (computerScore === 5) {
+        gameOver("computer");
+    } else {
+        return;
     }
 }
 
-// Play 5 rounds of rock, paper, scissors and tracks the score
+// Track score after each round
 
-function game() {
-    let draw = 0;
-    let playerWin = 0;
-    let computerWin = 0;
-    
-    for (let i = 0; i < 5; i++) {
-
-        let roundWinner = playRound();
-
-        if (roundWinner == "draw") {
-            draw++;
-        } else if (roundWinner == "player") {
-            playerWin++;
-        } else {
-            computerWin++;
-        }
-    }
-
-    if (playerWin == computerWin){
-        return "The final result: It's a draw! You both scored " + playerWin;
-    } else if (playerWin > computerWin) {
-        return "The final result: You win! You scored " + playerWin + " and the computer scored " + computerWin;
+function score(winner) {
+    if (winner === "draw") {
+        draw++;
+    } else if (winner === "player") {
+        playerScore++;
+        playerScoreboard.textContent = playerScore;
     } else {
-        return "The final result: You lost! You scored " + playerWin + " and the computer scored " + computerWin;
+        computerScore++;
+        computerScoreboard.textContent = computerScore;
     }
 }
 
-// console.log(game());
+// Display game over message
+
+function gameOver(finalWinner) {
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+
+    if (finalWinner === "player") {
+        finalMessage.textContent = "Congratulations! You beat the computer!";
+    } else if (finalWinner === "computer") {
+        finalMessage.textContent = "Better luck next time! The computer wins this time."
+    }
+}
+
+// UI
+
+const messageDiv = document.querySelector('.message');
+const message = document.createElement('p');
+messageDiv.appendChild(message);
+message.textContent = "Did you make the right choice?";
+
+const finalMessage = document.createElement ('h2');
+messageDiv.appendChild(finalMessage);
+finalMessage.textContent = " ";
+
+const playerDiv = document.querySelector('.player');
+const playerScoreboard = document.createElement('h3');
+playerDiv.appendChild(playerScoreboard);
+playerScoreboard.textContent = 0;
+
+const computerDiv = document.querySelector('.computer');
+const computerScoreboard = document.createElement('h3');
+computerDiv.appendChild(computerScoreboard);
+computerScoreboard.textContent = 0;
+
+const btn = document.querySelector('.button-group');
+const selection = btn.querySelectorAll('button');
+
+const rockBtn = selection[0];
+const paperBtn = selection[1];
+const scissorsBtn = selection[2];
+
+rockBtn.addEventListener("click", () => {
+    playRound("rock");
+});
+paperBtn.addEventListener("click", () => {
+    playRound("paper");
+});
+scissorsBtn.addEventListener("click", () => {
+    playRound("scissors");
+});
